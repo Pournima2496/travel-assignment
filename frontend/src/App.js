1d26079f28d39aa93902391aed4ff3ba5c44f451
Pournima2496 <pournima2496@gmail.com>
2023-03-10 03:27:42 +0530

    hero banner completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HeroBanner from './components/hero banners/HeroBanner';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
 
     <Navbar />
+    <HeroBanner />
     </div>
   );
 }
---
+++ frontend/src/components/hero banners/HeroBanner.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./hero.scss";
+import tourist from "../../assets/Layer 1 1.png";
+import worldMap from "../../assets/world_map_PNG28 3.png";
+import card1 from "../../assets/Rectangle 9.png";
+
+const HeroBanner = () => {
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <span>Visit</span>
+        <h1>
+          The Exotic<b>Lakshadweep</b>Islands
+        </h1>
+        <button>Discover Now</button>
+      </div>
+      <div className="hero-right">
+        <img src={tourist} alt="" className="tourist" />
+        <img src={worldMap} alt="" className="world_map" />
+        {/* <div className="circle1"/> */}
+        {/* <div className="circle2"/> */}
+        {/* <div className="circle3"/> */}
+
+        {/* Cards */}
+        <div className="card_lg">
+          <img src={card1} alt="" />
+          <strong>Explore Labuan Bajo</strong>
+          <span>NTT, Indonesua</span>
+        </div>
+        <div className="card_sm ">
+          <img src={card1} alt="" />
+          <strong>Explore Labuan Bajo</strong>
+          <span>NTT, Indonesua</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
